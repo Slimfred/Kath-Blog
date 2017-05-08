b3d1b8d6913619039a303a7486ed040a1dbe9268
Slimfred <slimfred37@gmail.com>
2017-05-08 17:27:49 -0400

    update specs autocreated

--- spec/helpers/info_pages_helper_spec.rb
+++ spec/helpers/info_pages_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe InfoPagesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe InfoPagesHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
