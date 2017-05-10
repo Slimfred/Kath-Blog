f994f02bef13cf12b11a85ed8c6f77ffdbcf1733
Slimfred <slimfred37@gmail.com>
2017-05-10 17:46:52 -0400

    add search box to nav bar

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,4 +8,8 @@ class Article < ApplicationRecord
 
   scope :created_at_desc, -> {order(created_at: :desc)}
 
+  def self.search(search_term)
+    Article.where("body LIKE ?", "%#{search_term}%")
+  end
+
 end
