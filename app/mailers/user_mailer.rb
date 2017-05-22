a74b2f8a86c0787f9d606d81afebb72c27d6d8cd
Slimfred <slimfred37@gmail.com>
2017-05-22 13:22:53 -0400

    add comments controller and views

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ApplicationMailer
   default from: "slimfred37@gmail.com"
+  @appname = "Kath's Blog"
 
     def contact_form(email, name, message)
     @message = message
@@ -9,9 +10,12 @@ class UserMailer < ApplicationMailer
     end
 
     def welcome(user)
-        @appname = "Kath's Blog"
         mail( :to => user.email,
               :subject => "Welcome to #{@appname}!")
     end
+
+    def new_article(user)
+      mail( :to => user.email,
+            :subject => "New article on #{@appname}!")
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'articles#index'
